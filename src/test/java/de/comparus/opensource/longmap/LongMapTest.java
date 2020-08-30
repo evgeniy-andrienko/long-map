f16e9af2e2c29d92e41600827ac1dde9f534544e
Evgeniy Andrienko <stelionser@gmail.com>
2020-08-30 19:11:59 +0300

    Added LongMap initialization functionality.

--- src/test/java/de/comparus/opensource/longmap/LongMapTest.java
+++ src/test/java/de/comparus/opensource/longmap/LongMapTest.java
@@ -2,13 +2,37 @@ package de.comparus.opensource.longmap;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LongMapTest {
 
     @Test
-    void testCase() {
-        assertTrue(true);
+    void initLongMapWithDefaultParams() {
+        new LongMapImpl<>();
+    }
+
+    @Test
+    void initLongMapWithCustomInitialCapacity() {
+        new LongMapImpl<>(100500);
+    }
+
+    @Test
+    void initLongMapWithIllegalCustomInitialCapacity() {
+        IllegalArgumentException illegalArgumentException = assertThrows(
+            IllegalArgumentException.class,
+            () -> new LongMapImpl<>(-1));
+
+        assertTrue(illegalArgumentException.getMessage().startsWith("Illegal initial capacity: "));
+    }
+
+    @Test
+    void initLongMapWithIllegalLoadFactor() {
+        IllegalArgumentException illegalArgumentException = assertThrows(
+            IllegalArgumentException.class,
+            () -> new LongMapImpl<>(100500, 0.0F));
+
+        assertTrue(illegalArgumentException.getMessage().startsWith("Illegal load factor: "));
     }
 
 }
